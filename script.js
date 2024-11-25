b76f4ad2a873d12aa41a68155107da4dacbeda2d
Alwin213 <alwinchacko2012@gmail.com>
2024-11-25 02:16:41 +0000

    Edit function added

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var selectedRow = NULL;
-function  onFormSubmit(){
+var selectedRow = null;
+function  onFormSubmit(e){
     event.preventDefault();
     var formData = readFormData();
-    if(selectedRow ===null){
+    if(selectedRow === null){
       insertNewRecord(formData);
     }
     else{
-
+          updateRecord(formData);
     }
-
+  resetForm();
 }
 function readFormData(){
     var formData = {};
@@ -31,7 +31,36 @@ var cell3= newRow.insertCell(2);
 var cell4= newRow.insertCell(3);
 cell4.innerHTML=data.Contactno;
 var cell5= newRow.insertCell(4);
-cell5.innerHTML='<button>Edit</button> <button>Delete </button>'
+cell5.innerHTML= '<button onClick='onEdit(this)'>Edit</button> <button> onClick ='onDelete(this)'>Delete</button>'
 }
 
+function OnEdit(td){
+ selectedRow =td.parentElement.parentElement;
+ document.getElementById('Typeoffarmingland').value=selectedRow.cells[0].innerHTML;
+ document.getElementById('Area').value=selectedRow.cells[0].innerHTML;
+ document.getElementById('Location').value=selectedRow.cells[0].innerHTML;
+ document.getElementById('Contactno').value=selectedRow.cells[0].innerHTML;
+}
 
+function updateRecord(formData){
+    selectedRow.cells[0].innerHTML=formData.Typeoffarmingland;
+    selectedRow.cells[0].innerHTML=formData.Area;
+    selectedRow.cells[0].innerHTML=formData.Location;
+    selectedRow.cells[0].innerHTML=formData.Contactno;
+
+}
+
+function onDelete(td){
+    if(confirm('Do you want to delete this record')){
+     row =td.parentElement.parentElement;
+    document.getElementById('storelist').deleteRow(row.rowIndex);
+}
+resetForm();
+}
+
+function resetForm(){
+     document.getElementById('Typeoffarmingland').value='';
+     document.getElementById('Area').value='';
+     document.getElementById('Location').value='';
+     document.getElementById('Contactno').value='';
+}
